2adfbd4983140aafe61a7c66d4cc2a1f3ff29e5f
Lucifixion <dqiytb@gmail.com>
2026-08-04 07:49:09 -0400

    Update ttt2_acrylic_hud.lua

--- lua/terrortown/lang/en/ttt2_acrylic_hud.lua
+++ lua/terrortown/lang/en/ttt2_acrylic_hud.lua
@@ -1,3 +1,3 @@
 L = LANG.GetLanguageTableReference("en")
 
-L.persistentWeaponSelect = "Enable persistent weapon selection"
+L.label_hud_persistent_weapon_select_enable = "Enable persistent weapon selection"
