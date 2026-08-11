49ffc7fe92954ccd3369e77f99d7c32daba8b2bd
Lucifixion <dqiytb@gmail.com>
2026-08-11 08:06:57 -0400

    avatar translation keys

--- lua/terrortown/lang/en/ttt2_acrylic_hud.lua
+++ lua/terrortown/lang/en/ttt2_acrylic_hud.lua
@@ -1,3 +1,4 @@
 L = LANG.GetLanguageTableReference("en")
 
 L.label_hud_persistent_weapon_select_enable = "Enable persistent weapon selection"
+L.label_hud_use_player_avatar = "Enable player avatar in spectator"
