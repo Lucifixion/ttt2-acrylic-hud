ed9316ec4c336a24b8884ccf8397a485995d704f
Lucifixion <dqiytb@gmail.com>
2026-08-27 15:44:52 -0400

    fix miniscoreboard errors on disconnect

--- lua/terrortown/gamemode/shared/hud_elements/tttminiscoreboard/acrylic_miniscoreboard.lua
+++ lua/terrortown/gamemode/shared/hud_elements/tttminiscoreboard/acrylic_miniscoreboard.lua
@@ -169,10 +169,12 @@ if CLIENT then
 			self:DrawBg(tmp_x, tmp_y, self.ply_ind_size, self.ply_ind_size, {r = ply_color.r, g = ply_color.g, b = ply_color.b, a = 120})
 			self:DrawSimpleOutline(tmp_x, tmp_y, self.ply_ind_size, self.ply_ind_size)
 
-			if ply:WasRevivedAndConfirmed() and ply:IsTerror() then
-				draw.FilteredTexture(tmp_x + 3, tmp_y + 3, self.ply_ind_size -6, self.ply_ind_size -6, self.icon_revived, 180, {r = 0,g = 0,b = 0})
-			elseif ply:OnceFound() and not ply:RoleKnown() then -- draw marker on indirect confirmed bodies
-				draw.FilteredTexture(tmp_x + 3, tmp_y + 3, self.ply_ind_size -6, self.ply_ind_size -6, self.icon_in_conf, 120, {r = 0,g = 0,b = 0})
+			if IsValid(ply) then
+				if ply:WasRevivedAndConfirmed() and ply:IsTerror() then
+					draw.FilteredTexture(tmp_x + 3, tmp_y + 3, self.ply_ind_size -6, self.ply_ind_size -6, self.icon_revived, 180, {r = 0,g = 0,b = 0})
+				elseif ply:OnceFound() and not ply:RoleKnown() then -- draw marker on indirect confirmed bodies
+					draw.FilteredTexture(tmp_x + 3, tmp_y + 3, self.ply_ind_size -6, self.ply_ind_size -6, self.icon_in_conf, 120, {r = 0,g = 0,b = 0})
+				end
 			end
 		end
 	end
